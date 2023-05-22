ac9dfc3054791d780c4188681d7738a4e5f0d8dd
Ritul Soni <rsoni27@illinois.edu>
2023-05-21 22:28:33 -0500

    home

---
+++ my-app/src/Home/home.js
@@ -0,0 +1,14 @@
+// Home.js
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Welcome to My Personal Portfolio</h1>
+      <p>This is a placeholder for some cool feature. You can replace this later!</p>
+    </div>
+  );
+};
+
+export default Home;
