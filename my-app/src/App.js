d61849ef37ba8dadfc713007b340ba69e2de08a7
Ritul Soni <rsoni27@illinois.edu>
2023-05-21 21:55:42 -0500

    Created Base Features

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar/NavBar';
+import Home from './Home/home.js';
 
 export default function App() {
+  const [showSpline, setShowSpline] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSpline(false);
+      setShowButton(true);
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClick = () => {
+    navigate("/home");
+    setButtonClicked(true);
+  }
+
   return (
     <div className="App">
       <NavBar />
-      <div className="App-main">
-        <Spline scene="https://prod.spline.design/Z9vReQa5Z8pP47z5/scene.splinecode" />
-      </div>
-      <header className="App-header">
-        <h1>Welcome to My Personal Portfolio</h1>
-      </header>
-      <main className="App-main">
-        <p>This is a placeholder for some cool feature. You can replace this later!</p>
-      </main>
+
+      {showSpline && 
+        <div className="App-spline">
+          <Spline scene="https://prod.spline.design/MEHV8aAVdFEvGaAN/scene.splinecode" />
+        </div>
+      }
+
+      {showButton && !buttonClicked && 
+        <button className="transparent-button" onClick={handleClick}>
+          <Spline scene="https://prod.spline.design/xDv3ZhzpGvGIxhGj/scene.splinecode" />
+        </button>
+      }
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
